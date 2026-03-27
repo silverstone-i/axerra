e44cd2549f0b3b4c7aa213d72d6f582e0622dea1
ian <ian@isilverstone.com>
2026-03-27 15:30:43 -0400

    feat: add email search and user type filters to Manage Users page
    Extend ModuleBar to support select-type filters (options array renders
    TextField as a dropdown). Add email substring search and entity_type
    dropdown filter to ManageUsersPage with client-side filtering.

--- apps/nap-client/src/components/layout/ModuleBar.jsx
+++ apps/nap-client/src/components/layout/ModuleBar.jsx
@@ -16,6 +16,7 @@ import {
   Link,
   Typography,
   Button,
+  MenuItem,
   TextField,
   ToggleButtonGroup,
   ToggleButton,
@@ -155,11 +156,18 @@ export default function ModuleBar() {
             variant="outlined"
             value={filter.value || ''}
             onChange={filter.onChange}
+            select={!!filter.options}
             sx={{
               minWidth: 120,
               '& .MuiInputBase-input': { py: 0.5, ...FONT.toolbarAction },
             }}
-          />
+          >
+            {filter.options?.map((opt) => (
+              <MenuItem key={opt.value} value={opt.value}>
+                {opt.label}
+              </MenuItem>
+            ))}
+          </TextField>
         ))}
 
         {/* Primary actions */}
